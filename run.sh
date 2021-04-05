6186138761d4b00d1f1eeb27de9e83b9f60b9161
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2021-04-05 16:59:05 +0530

    Update run.sh
    use gnome-terminal for output

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ EXT=$(basename $GEDIT_CURRENT_DOCUMENT_NAME|sed 's/.*\(\..*\)/\1/')
 echo $EXT
 
 if   [ $EXT = '.c' ] || [ $EXT = '.cpp' ]; then
-     make $DOCUMENT && ./$DOCUMENT
+     make $DOCUMENT && gnome-terminal -- bash -c ./$DOCUMENT
 elif [ $EXT = '.sh' ]; then
      sh $GEDIT_CURRENT_DOCUMENT_NAME
 elif [ $EXT = '.py' ]; then
@@ -13,5 +13,5 @@ elif [ $EXT = '.py' ]; then
      if [ ! -x $PYTHON ]; then 
            echo ""Python Interpreter not found""
       fi
-      $PYTHON $GEDIT_CURRENT_DOCUMENT_NAME
+      gnome-terminal -- bash -c "$PYTHON $GEDIT_CURRENT_DOCUMENT_NAME"
 fi
